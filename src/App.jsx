5306f000b681f6989042c43f728c4ba47dded7c1
DandiRizkyy <dandirizkyy94@gmail.com>
2026-08-29 13:10:50 +0700

    cleaning grid style

--- src/App.jsx
+++ src/App.jsx
@@ -233,7 +233,6 @@ function App() {
       <div className="hero-bg">
         <div className="gradient-orb orb-1"></div>
         <div className="gradient-orb orb-2"></div>
-        <div className="grid-overlay"></div>
       </div>
 
       {/* Navigation */}
